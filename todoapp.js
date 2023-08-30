5f9a20f2e5b9032e0523d659fe6b3da95e1b62e7
AhmerAli17 <ahmerali1771@gmail.com>
2023-08-30 14:41:49 +0100

    first commit

---
+++ todoapp.js
@@ -0,0 +1,60 @@
+import inquirer from 'inquirer';
+//todos= done
+//function=done
+//opreation=?
+let todos = ["Ahmer", "Mamoon", "Abdul Basit", "Anas"];
+async function CreatTodos(todos) {
+    do {
+        const ans = await inquirer.prompt({
+            type: "list",
+            message: "Select the options ",
+            name: "slect",
+            choices: ["add", "update", "view", "delete"]
+        });
+        if (ans.slect == "add") {
+            const addTodos = await inquirer.prompt({
+                type: "input",
+                message: "Add item",
+                name: "todo"
+            });
+            todos.push(addTodos.todo);
+            console.log(todos);
+        }
+        ;
+        if (ans.slect == "update") {
+            const updateTodo = await inquirer.prompt({
+                type: "list",
+                message: "Select Update item",
+                name: "update",
+                choices: todos.map(item => item),
+            });
+            const addTodos = await inquirer.prompt({
+                type: "input",
+                message: "Add New item",
+                name: "todo"
+            });
+            let newTodo = todos.filter(val => val !== updateTodo.update);
+            todos = [...newTodo, addTodos.todo];
+            console.log(todos);
+        }
+        ;
+        if (ans.slect == "view") {
+            console.log(todos);
+        }
+        ;
+        if (ans.slect == "delete") {
+            const deleteTodo = await inquirer.prompt({
+                type: "list",
+                message: "Select Delete item",
+                name: "update",
+                choices: todos.map(item => item),
+            });
+            let newTodo = todos.filter(val => val !== deleteTodo.update);
+            todos = [...newTodo];
+            console.log(todos);
+        }
+        ;
+    } while (true);
+}
+;
+CreatTodos(todos);
